5e41125b747a691dfed4d40808d381a209c31007
Alexander James E. Lunas <lunasalexander41@gmail.com>
2024-11-22 01:26:25 +0800

    I'm done now with the overall structure of this program so basically I finished the algorithm already and I quite struggle to how I will print the personal informations, so I refer again to the internet and found a reliable source; that in my for loop, I can use the range method and len method to iterate each elements in the array (their indices and corresponding value). But majority is from my professor because we already discuss this coding topic about text file integration. I will now just gonna do some modifications here for preference only, like more contextualization.

--- display_the_personal_informations_based_on_the_given_name.py
+++ display_the_personal_informations_based_on_the_given_name.py
@@ -1,4 +1,16 @@
+call_name = input("Enter the name: ") 
+final_call_name = "Name: " + call_name
+
 with open("C:\\Users\\Alexander\\OneDrive\\Desktop\\one_or_more_entries_for_personal_informations_with_integration_of_text_file\\user(s)_informations.txt", "r") as database_2:
     lines = database_2.readlines()
-    for line in lines:
-        print(line.strip())
+    for line in range(len(lines)):
+        if lines[line].strip() == final_call_name:
+            print(lines[line].strip())
+            print(lines[line + 1].strip())
+            print(lines[line + 2].strip())
+            print(lines[line + 3].strip())
+            print(lines[line + 4].strip())
+            print(lines[line + 5].strip())
+            break
+    else:
+        print(f"'{call_name}' was not found in the file.")
